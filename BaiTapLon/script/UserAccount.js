0d96f5bd29d31ccf5def16aa07dc844694cc84f0
thanhdang3105 <thanhls1235ls12@gmail.com>
2023-03-07 10:30:33 +0700

    Add project files.

---
+++ BaiTapLon/script/UserAccount.js
@@ -0,0 +1,60 @@
+﻿(function handleInputChange() {
+    const listInput = document.querySelectorAll('div.wrapper_body input')
+    for (let input of listInput) {
+        input.oninput = (event) => {
+            if (event.target.value) {
+                setErrorInfo(event.target.name, undefined, false)
+            }
+        }
+    }
+})()
+
+function setErrorInfo(inputName, errMsg = 'Error', isErr = true) {
+    const input = document.querySelector(`input[name=${inputName}]`)
+    if (input) {
+        const wrapperInput = input.parentElement
+        if (isErr) {
+            if (wrapperInput) {
+                wrapperInput.classList.add('showError')
+                const spanErr = wrapperInput.querySelector('div.error_info > span')
+                if (spanErr) {
+                    spanErr.innerText = errMsg
+                }
+            }
+        } else {
+            if (wrapperInput) {
+                wrapperInput.classList.remove('showError')
+                const spanErr = wrapperInput.querySelector('div.error_info > span')
+                if (spanErr) {
+                    spanErr.innerText = errMsg
+                }
+            }
+        }
+        
+    }
+}
+
+function toggleAction() {
+    const switchView = document.querySelector('div.switch_view');
+    const formLogin = document.querySelector('form#login');
+    const formRegister = document.querySelector('form#register');
+    if (switchView) {
+        if (switchView.classList.contains('right')) {
+            switchView.classList.remove('right')
+            switchView.innerText = 'Chào mừng bạn trở lại với Web Sách'
+            resetForm(formRegister)
+        } else {
+            switchView.classList.add('right')
+            switchView.innerText = 'Tạo tài khoản để trở thành 1 thành viên của Web Sách'
+            resetForm(formLogin)
+        }
+    }
+}
+
+function resetForm(form) {
+    form.reset();
+    const listInputErr = form.querySelectorAll('.wrapper_input.showError input');
+    for (let input of listInputErr) {
+        setErrorInfo(input.name, undefined, false)
+    }
+}
---
+++ BaiTapLon/server/HandleAccount.aspx.cs
@@ -0,0 +1,137 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace BaiTapLon.server
+{
+    public partial class HandleAccount : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlDB"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            String action = Request.QueryString["action"];
+            switch (action) {
+                case "register":
+
+                    break;
+                case "login":
+                    String userName = Request.Form["username"];
+                    String password = Request.Form["password"];
+                    Login(userName, password);
+                    break;
+                default:
+                    Response.ContentType = "application/json";
+                    Response.StatusCode = 400;
+                    Response.Write("{\"msg\":\"Action not supported with route!\"}");
+                    Response.End();
+                    break;
+            }
+
+            con.Close();
+        }
+
+        public class user
+        {
+            public int ID { get; set; }
+            public string username { get; set; }
+            public string birthday { get; set; }
+            public string sex { get; set; }
+            public user(int id, string userName, string birthDay, string Sex)
+            {
+                this.ID = id;
+                this.username = userName;
+                this.birthday = birthDay;
+                this.sex = Sex;
+            }
+            public string converString ()
+            {
+                return "{\"id\":\"" + this.ID + "\", \"username\":\"" +this.username +"\",\"birthDay\":\""+this.birthday+"\"" +
+                    ",\"sex\":\""+this.sex+"\"}";
+            }
+        }
+
+        private void Login(string username, string password)
+        {
+            Response.ContentType = "application/json";
+            string errorMsg = "Login Failed!";
+            if (username != null && password != null)
+            {
+                string encode = encodePassword(password);
+
+                string decode = dencodePassword(encode);
+
+                SqlCommand cmd = new SqlCommand("select * from Auth where username='"+username+"'", con);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                string userID = null;
+
+
+
+                while (reader.Read())
+                {
+                    if (reader[1].ToString() == username && reader[2].ToString() == password)
+                    {
+                        userID = reader[3].ToString();
+                    }
+                }
+
+                reader.Close();
+
+                user userInfo = null;
+                if (userID != null)
+                {
+                    cmd = new SqlCommand("select * from Users where ID=" + userID, con);
+
+                    reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        userInfo = new user((int)reader["ID"], reader["name"].ToString(), reader["birthday"].ToString(), reader["sex"].ToString());
+                    }
+
+
+                }
+                if (userInfo != null)
+                {
+                    Response.Write("{\"data\":" + userInfo.converString() + "}");
+                    Response.End();
+                }
+                else
+                {
+                    errorMsg = "Sai tên đăng nhập hoặc mật khẩu!";
+                }
+            }
+            Response.StatusCode = 400;
+            Response.Write("{\"msg\":\""+ errorMsg +"\"}");
+            Response.End();
+        }
+
+        private string encodePassword(string password)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(password);
+
+            string encoded = Convert.ToBase64String(bytes);
+            return encoded;
+        }
+        private string dencodePassword(string passwordEncoded)
+        {
+            byte[] bytes = Convert.FromBase64String(passwordEncoded);
+
+            string decoded = Encoding.UTF8.GetString(bytes);
+            return decoded;
+        }
+    }
+}
